fb434a0d70193edece285969e07d39b4e4cb0439
Bakamura <ra00297288@pucsp.edu.br>
2021-10-06 17:37:33 -0300

    More Hud experimentation

---
+++ Assets/Script/Geral/Player/PlayerHUD.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour {
+
+    private PlayerData dataScript;
+    public Image healthBar;
+
+    private PlayerAttack atkScript;
+    public Image atk0ImageCDown;
+    public Image atk1ImageCDown;
+    public Image atk2ImageCDown;
+
+    private void Start() {
+        dataScript = GetComponent<PlayerData>();
+        atkScript = GetComponent<PlayerAttack>();
+    }
+
+    private void FixedUpdate() {
+        DataUI();
+        AtkUI();
+    }
+
+    private void DataUI() {
+        
+    }
+
+    private void AtkUI() {
+        atk0ImageCDown.fillAmount = (atkScript.atk0TotalCDown - atkScript.atk0CDown) / atkScript.atk0TotalCDown;
+        atk1ImageCDown.fillAmount = (atkScript.atk1TotalCDown - atkScript.atk1CDown) / atkScript.atk0TotalCDown;
+        atk2ImageCDown.fillAmount = (atkScript.atk2TotalCDown - atkScript.atk2CDown) / atkScript.atk0TotalCDown;
+    }
+
+}
