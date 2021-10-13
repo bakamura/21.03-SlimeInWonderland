2f0a0918a07bef6f7810e5ef46fe615d203aaf29
João Vitor <mendesjvm@hotmail.com>
2021-10-13 11:43:03 -0300

    Mudança no script base dos inimigos

--- Assets/Script/Geral/Enemy/EnemyBase.cs
+++ Assets/Script/Geral/Enemy/EnemyBase.cs
@@ -5,27 +5,44 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour {
     [Header("Components")]
     private Animator animatorEnemy;
+    private Collider2D colliderEnemy;
+    private PlayerAttack playerAtkScript;
 
     [Header("Stats")]
     public float maxHealth;
     private float currentHealth;
+    public int xpType;
+    public float xpAmount;
 
     [System.NonSerialized] public bool isSpecialCase = false;
     [System.NonSerialized] public bool specialCaseTrigger = false;
 
     private void Start() {
         animatorEnemy = GetComponent<Animator>();
+        colliderEnemy = GetComponent<Collider2D>();
+        playerAtkScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>();
         currentHealth = maxHealth;
     }
 
     public void TakeDamage(float damage) {
         if (!isSpecialCase) currentHealth -= damage;
         else specialCaseTrigger = true;
-        if (currentHealth <= 0) animatorEnemy.SetBool("Die", true);
+        if (currentHealth <= 0) {
+            animatorEnemy.SetBool("Die", true);
+            colliderEnemy.isTrigger = true;
+        }
         if (currentHealth > maxHealth) currentHealth = maxHealth;
     }
 
     public void DestroyObject() {
         Destroy(gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        playerAtkScript.xpToGet[xpType] += xpAmount;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        playerAtkScript.xpToGet[xpType] -= xpAmount;
     }
 }
