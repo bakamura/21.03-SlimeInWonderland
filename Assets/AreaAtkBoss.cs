cac093a24d8a0200eb29ae3c5c0f599618960891
João Vitor <mendesjvm@hotmail.com>
2021-11-03 10:14:55 -0300

    Boss
    Boss ta pulando e atirando, falta atk area e o resto da animação

---
+++ Assets/AreaAtkBoss.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaAtkBoss : MonoBehaviour {
+
+    public float areaForce;
+    public float areaDamage;
+
+    private void Start() {
+        GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(3, 3));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        
+    }
+
+
+
+}
---
+++ Assets/Script/Geral/Enemy/FireBallBoss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallBoss : MonoBehaviour {
+
+    public float shotForce;
+    public float damageShot;
+
+    private void Start() {
+        GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.down * shotForce);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") collision.GetComponent<PlayerData>().TakeDamage(damageShot);
+    }
+
+
+
+
+
+}
