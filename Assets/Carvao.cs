bc4f92c3c7668132f98d5534778e9560d3f17e59
João Vitor <mendesjvm@hotmail.com>
2021-11-17 10:15:05 -0300

    Carvao e tiro carvao arrumados
    agora ele corre do player e o tiro tem animacao

--- Assets/Carvao.cs
+++ Assets/Carvao.cs
@@ -25,6 +25,8 @@ public class Carvao : MonoBehaviour {
     public float shotForce;
     public float atkRange;
 
+    private bool inWall;
+
     private void Start() {
         rbCarvao = GetComponent<Rigidbody2D>();
         animCarvao = GetComponent<Animator>();
@@ -39,8 +41,8 @@ public class Carvao : MonoBehaviour {
     }
 
     private void AtkPatern() {
+        Vector2 direction = (transform.position - playerTransform.position).normalized;
         if (!resting && !baseScript.beingKb) {
-            Vector2 direction = (playerTransform.position - transform.position).normalized;
             rbCarvao.velocity = direction * speed;
             patrolScript.facing = direction;
 
@@ -48,13 +50,15 @@ public class Carvao : MonoBehaviour {
             animCarvao.SetFloat("Horizontal", direction.x);
             animCarvao.SetFloat("Vertical", direction.y);
 
-            if (Vector2.Distance(transform.position, playerTransform.position) < followGap) {
+            if (Vector2.Distance(transform.position, playerTransform.position) > followGap || inWall) {
                 resting = true;
                 animCarvao.SetTrigger("Shot");
                 StartCoroutine(InstantiateShots());
             }
         }
         else {
+            animCarvao.SetFloat("Horizontal", -direction.x);
+            animCarvao.SetFloat("Vertical", -direction.y);
             if (!baseScript.beingKb) rbCarvao.velocity = Vector2.zero;
             animCarvao.SetBool("Moving", false);
         }
@@ -77,23 +81,20 @@ public class Carvao : MonoBehaviour {
         go.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.left * shotForce);
         go.GetComponent<FireBallBoss>().damageShot = shotDamage;
 
-        yield return new WaitForSeconds(shotDelay);
-
-        animCarvao.SetTrigger("Shot");
-
-        yield return new WaitForSeconds(0.6875f);
-
-        a = Mathf.Atan2(playerTransform.transform.position.y - transform.position.y, playerTransform.transform.position.x - transform.position.x) * Mathf.Rad2Deg + 180;
-        go = Instantiate(shotGObject, transform.position, Quaternion.Euler(0, 0, a));
-        go.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.left * shotForce);
-        go.GetComponent<FireBallBoss>().damageShot = shotDamage;
-
         yield return new WaitForSeconds(restingTime);
 
         resting = false;
     }
 
-        private void OnDrawGizmos() {
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag == "Wall") inWall = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.collider.tag == "Wall") inWall = false;
+    }
+
+    private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         if (Application.isPlaying) Gizmos.DrawWireSphere(transform.position + (new Vector3(patrolScript.facing.x, patrolScript.facing.y, 0)) / 2, atkRange);
     }
--- Assets/Script/Geral/Enemy/FireBallBoss.cs
+++ Assets/Script/Geral/Enemy/FireBallBoss.cs
@@ -14,8 +14,24 @@ public class FireBallBoss : MonoBehaviour {
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.tag == "Player") collision.GetComponent<PlayerData>().TakeDamage(damageShot);
-        else if (collision.tag == "Litable") collision.GetComponent<Torch>().ChangeState(true);
-        else if (collision.tag == "Wall") Destroy(gameObject);
+        if (collision.tag == "Player") {
+            collision.GetComponent<PlayerData>().TakeDamage(damageShot);
+            StartCoroutine(ExplosionTime());
+        }
+        else if (collision.tag == "Litable") {
+            collision.GetComponent<Torch>().ChangeState(true);
+            StartCoroutine(ExplosionTime());
+        }
+        else if (collision.tag == "Wall") StartCoroutine(ExplosionTime());
+    }
+
+    IEnumerator ExplosionTime() {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<Animator>().SetTrigger("Explosion");
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        yield return new WaitForSeconds(0.5f);
+
+        Destroy(gameObject);
     }
 }
